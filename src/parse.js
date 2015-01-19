8f302aa368c2e4377507e61689b258e52be0d398
Ryan Ashcraft <ryan@ryanashcraft.me>
2015-01-19 15:56:29 -0800

    Add support to parse Jekyll-style Markdown

--- src/parse.js
+++ src/parse.js
@@ -3,6 +3,7 @@
 var path = require('path');
 var json3 = require('json3');
 var toml = require('toml');
+var yaml = require('js-yaml');
 
 function parse(file) {
     var extension = path.extname(file.path);
@@ -12,9 +13,35 @@ function parse(file) {
             return json3.parse(file.contents.toString());
         case '.toml':
             return toml.parse(file.contents.toString());
+        case '.md':
+        case '.markdown':
+            return parseMarkdown(file);
         default:
             throw new Error('Unsupported extension ' + extension);
     }
+}
+
+function parseMarkdown(file) {
+    var s = file.contents.toString();
+    var delimeter = '---';
+    var frontMatterStartDelIndex = s.indexOf(delimeter);
+    var frontMatterStopDelIndex = s.indexOf(delimeter, delimeter.length);
+
+    if (
+        frontMatterStartDelIndex === -1 ||
+        frontMatterStopDelIndex === -1 ||
+        frontMatterStopDelIndex <= frontMatterStartDelIndex
+    ) {
+        throw new Error('Failed to parse ' + file.relative + ' (failed to parse YAML front matter)');
+        return;
+    }
+
+    var frontMatter = s.substring(frontMatterStartDelIndex, frontMatterStopDelIndex);
+    var data = yaml.load(frontMatter);
+    var body = s.substring(frontMatterStopDelIndex + delimeter.length);
+    data.body = body;
+
+    return data;
 }
 
 module.exports = parse;
